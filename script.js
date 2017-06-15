b25002c116b1068fca8aeb1bd3395e202e076a91
Kreuter97 <cjkreu@gmail.com>
2017-06-15 13:20:13 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$('form').submit(function() {
-    // post the form data to the spreadsheet
-    $.post('https://script.google.com/macros/s/AKfycbyOyFG6fPlMOZi6Biv8Df9qIY46_DYWK4YiPbR8hl-nfb7omMs/exe', $('form').serialize());
-    
-    // reset the value on the input
-    $('input[type=text]').val('');
-    return false;
-});
