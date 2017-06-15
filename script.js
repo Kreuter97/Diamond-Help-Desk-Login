a40b0411a7a4310057b7eab2ab28a6a1c52f6a4d
Kreuter97 <cjkreu@gmail.com>
2017-06-15 12:48:16 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+$('form').submit(function() {
+    // post the form data to the spreadsheet
+    $.post('https://script.google.com/macros/s/AKfycbyOyFG6fPlMOZi6Biv8Df9qIY46_DYWK4YiPbR8hl-nfb7omMs/exec', $('form').serialize());
+    
+    // reset the value on the input
+    $('input[type=text]').val('');
+    return false;
+});
